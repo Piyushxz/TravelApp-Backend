6e52e857a6573e6b4ef7a8bb563b7d5641a01bb1
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-07-02 16:39:16 +0530

    category issue

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,26 +1,17 @@
 const Hotel = require("../model/hotel.model")
 const getAllHotelHandler = async (req, res) => {
-    const hotelCategory = req.query.hotelCategory;
-    try {
-        let hotels;
-        if (hotelCategory) {
-            // Find hotels by category
-            hotels = await Hotel.find({ category: hotelCategory });
-        } else {
-            // Find all hotels
+    const hotelCategory = req.query.category
+    try{
+        let hotels
+        if (hotelCategory){
+            hotels = await Hotel.find({ category: hotelCategory })
+        }else{
             hotels = await Hotel.find({});
         }
-
-        // Check if hotels are found
-        if (hotels.length > 0) {
-            res.json(hotels);
-        } else {
-            res.status(404).json({ message: "No data found" });
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Error fetching hotels" });
+        hotels ? res.json(hotels) : res.status(404).json({ message: "No data found" })
+    }catch(err){
+        console.log(err)
     }
 }
 
-module.exports = getAllHotelHandler
+module.exports = getAllHotelHandler;
